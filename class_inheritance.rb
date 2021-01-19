516067a91be58fd3e13b2b856f770557f355187b
rafael silva <silvaserranor@gmail.com>
2021-01-19 14:40:27 -0500

    Created Employee and Managegr classes

---
+++ class_inheritance.rb
@@ -0,0 +1,24 @@
+class Employee
+    attr_reader :name, :title, :salary, :boss
+    
+    def initialize (name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
+    end
+
+    def bonus(multiplier)
+        @bonus = (self.salary) * multiplier
+    end
+end
+
+class Manager < Employee
+    def initialize(name, title, salary, boss) 
+        super(name, title, salary, boss)
+        @Employees = []
+    end
+
+end
+
+
