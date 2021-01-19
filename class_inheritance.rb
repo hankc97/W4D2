87591d0376dd7a90024e992d81bccc3f209b9808
Hank <hankchen1997@yahoo.com>
2021-01-19 15:02:14 -0500

    Added Manager bonus

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -14,11 +14,29 @@ class Employee
 end
 
 class Manager < Employee
+    attr_reader :employees
     def initialize(name, title, salary, boss) 
         super(name, title, salary, boss)
-        @Employees = []
+        @employees = []
+    end
+
+    def hire
+
+
+    end
+
+    def bonus(multiplier)
+        employees_salary = 0
+
+        @employees.each {|employee| employees_salary+=employee.salary}
+        
+        @bonus = employees_salary * multiplier
     end
 
 end
 
 
+david = Employee.new("David", "TA", 10000, "Darren")
+shawna = Employee.new("Shawna", "TA", 12000 ,"Darren")
+darren = Manager.new("Darren", "TA Manager", 78000, "Ned")
+ned = Manager.new("Ned", "Founder", 1000000, nil)
