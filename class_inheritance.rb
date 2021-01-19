9a353dc62fad52a14594743dac2f19a9824bb539
rafael silva <silvaserranor@gmail.com>
2021-01-19 15:09:18 -0500

    conflict

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -1,41 +1,29 @@
 class Employee
     attr_reader :name, :title, :salary, :boss
-    
     def initialize (name, title, salary, boss)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
     end
-
     def bonus(multiplier)
         @bonus = (self.salary) * multiplier
     end
 end
-
 class Manager < Employee
     attr_reader :employees
     def initialize(name, title, salary, boss) 
         super(name, title, salary, boss)
         @employees = []
     end
-
     def hire
-
-
     end
-
     def bonus(multiplier)
         employees_salary = 0
-
         @employees.each {|employee| employees_salary+=employee.salary}
-        
         @bonus = employees_salary * multiplier
     end
-
 end
-
-
 david = Employee.new("David", "TA", 10000, "Darren")
 shawna = Employee.new("Shawna", "TA", 12000 ,"Darren")
 darren = Manager.new("Darren", "TA Manager", 78000, "Ned")
