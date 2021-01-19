687a69e321f7f880527f9d891b91fd4b5751f015
rafael silva <silvaserranor@gmail.com>
2021-01-19 16:13:11 -0500

    added bonus

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -9,34 +9,48 @@ class Employee
     end
 
     def bonus(multiplier)
-        @bonus = (self.salary) * multiplier
+        bonus = (self.salary) * multiplier
     end
 end
 
 class Manager < Employee
-    attr_reader :employees
+    attr_accessor :employees
     def initialize(name, title, salary, boss) 
         super(name, title, salary, boss)
         @employees = []
     end
 
-    def hire
-
-
+    def hire(employee)
+        self.employees.push(employee)
     end
 
     def bonus(multiplier)
         employees_salary = 0
 
-        @employees.each {|employee| employees_salary+=employee.salary}
-        
-        @bonus = employees_salary * multiplier
+        @employees.each do |employee|
+            if employee.kind_of?(Manager)
+                employees_salary += employee.employees.bonus(multiplier)
+                employees_salary += self.salary
+            else
+                employees_salary+=employee.salary
+            end
+        end 
+        bonus = employees_salary * multiplier
     end
 
 end
 
-
+ned = Manager.new("Ned", "Founder", 1000000, nil)
 david = Employee.new("David", "TA", 10000, "Darren")
 shawna = Employee.new("Shawna", "TA", 12000 ,"Darren")
 darren = Manager.new("Darren", "TA Manager", 78000, "Ned")
-ned = Manager.new("Ned", "Founder", 1000000, nil)
+
+ned.hire(david)
+darren.hire(shawna)
+darren.hire(david)
+puts "ned bonus"
+p ned.bonus(5)
+p "darren"
+p darren.bonus(4)
+p "david"
+p david.bonus(3)
